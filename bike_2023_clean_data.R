f2683b24e5696c582bf769d5f29d6d51662ead5c
Nipphit Apisitpuwakul <nipphit.api@student.mahidol.ac.th>
2024-03-20 15:18:29 +0700

    feat: update cleaned dataset

--- bike_2023_clean_data.R
+++ bike_2023_clean_data.R
@@ -10,9 +10,21 @@ library("dplyr")
 library("lubridate")
 library("hms")
 
-setwd("/Users/nipphittt/Documents/DA_Project/GoogleAnalytics/Project_Track1/")
-bike_data <- read_csv("Divvy_Trips_2020_Q1.csv")
+setwd("/Users/nipphittt/Documents/DA_Project/GoogleAnalytics/Project_Track1/2023_data/")
+bike_data_1 <- read_csv("202301-divvy-tripdata.csv")
+bike_data_2 <- read_csv("202302-divvy-tripdata.csv")
+bike_data_3 <- read_csv("202303-divvy-tripdata.csv")
+bike_data_4 <- read_csv("202304-divvy-tripdata.csv")
+bike_data_5 <- read_csv("202305-divvy-tripdata.csv")
+bike_data_6 <- read_csv("202306-divvy-tripdata.csv")
+bike_data_7 <- read_csv("202307-divvy-tripdata.csv")
+bike_data_8 <- read_csv("202308-divvy-tripdata.csv")
+bike_data_9 <- read_csv("202309-divvy-tripdata.csv")
+bike_data_10 <- read_csv("202310-divvy-tripdata.csv")
+bike_data_11 <- read_csv("202311-divvy-tripdata.csv")
+bike_data_12 <- read_csv("202312-divvy-tripdata.csv")
 
+bike_data <- rbind(bike_data_1,bike_data_2,bike_data_3,bike_data_4,bike_data_5,bike_data_6,bike_data_7,bike_data_8,bike_data_9,bike_data_10,bike_data_11,bike_data_12)
 skim_without_charts(bike_data)
 
 # Check duplicate
@@ -23,10 +35,18 @@ print(unique(bike_data$rideable_type))
 print(unique(bike_data$start_station_name))
 print(unique(bike_data$end_station_name))
 
-check_null <- bike_data %>% filter(is.na(end_station_name))
 # Filter the record with no ended station (id, name, and time) out
 bike_data <- bike_data %>% 
+  filter(!is.na(end_station_id))
+bike_data <- bike_data %>% 
   filter(!is.na(end_station_name))
+check_null <- bike_data %>% filter(is.na(end_station_name))
+
+bike_data <- bike_data %>% 
+  filter(!is.na(start_station_id))
+bike_data <- bike_data %>% 
+  filter(!is.na(start_station_name))
+check_null <- bike_data %>% filter(is.na(start_station_name))
 
 # Create new column named ride_length (ended_at - started_at) for recording time ride.
 bike_data <- bike_data %>% 
@@ -43,10 +63,9 @@ bike_data <- bike_data %>%
 filtered_check2 <- filter(bike_data, ended_at < started_at)
 
 # Remove rows that ride_length are less than 1 minute
-filtered_check3 <- filter(bike_data, difftime(ended_at, started_at) < 60)
 bike_data <- bike_data %>% 
   filter(!(difftime(ended_at, started_at) < 60))
-
+filtered_check3 <- filter(bike_data, difftime(ended_at, started_at) < 60)
 
 # Create new column named day_of_week (Sunday = 1, Monday = 2, ...)
 bike_data <- bike_data %>% 
@@ -57,6 +76,6 @@ bike_data <- bike_data %>%
   mutate(month = month(started_at))
 
 # Import the cleaned data as .csv file
-write.csv(bike_data, file = "bike_data_cleaned.csv")
+write.csv(bike_data, file = "bike_data_2023_cleaned.csv")
 
-
+bike_data_test <- read_csv("bike_data_2023_cleaned.csv")
